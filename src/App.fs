ab4e22b52117d1026d477a59419200432682acc4
Diego Esmerio <menhowca@gmail.com>
2017-09-20 22:41:26 -0300

    Game actions started

--- src/App.fs
+++ src/App.fs
@@ -10,8 +10,12 @@ let (|DivRem|) a n =
 let (|Float|) a = float a
 
 
-type 'a Point = {x: ^a; y: ^a}
-type 'a Size = {w: ^a; h: ^a}
+type 'a Point =
+    {x: ^a; y: ^a}
+    static member Empty = {x=Unchecked.defaultof<'a>;y=Unchecked.defaultof<'a>}
+type 'a Size =
+    {w: ^a; h: ^a}
+    static member Empty = {w=Unchecked.defaultof<'a>;h=Unchecked.defaultof<'a>}
 
 
 type 'a Rect =
@@ -19,37 +23,43 @@ type 'a Rect =
         p : 'a Point
         s : 'a Size
     }
+    static member Empty = {p=Point<'a>.Empty;s=Size<'a>.Empty}
+
 let inside {p={x=x;y=y};s={h=h;w=w}} {x=x';y=y'} =        
         x' >= x && x' <= ( x + w ) && y' >= y && y' <= ( y + h )
 
 
-type Card(order)=
-    let (Float kind) = order%13;
-    let (Float suit) = order/13;
-    let selected = ref false
-    static let deck =
+type Card=
+    {
+        Kind: int
+        Suit: int
+        Selected: bool
+        LastRendering: float Rect
+    }    
+    static member private deck =
         let image = Browser.document.createElement_img()
         image.src <- "Deck.svg"
         image
-    static let back =
+    static member private back =
         let image = Browser.document.createElement_img()
         image.src <- "Back.svg"
         image
-    member val IsSelected = !selected
+    member t.IsSelected = t.Selected
     member t.Toggle () =
-        selected:= t.IsSelected |> not
-        t.IsSelected
+        {t with Selected = not t.Selected}
     static member DrawTo ((ctx:Browser.CanvasRenderingContext2D),{p={x=x; y=y};s={w=w;h=h}}) =
-        ctx.drawImage(!^back,x,y,w,h)
-    member t.DrawTo((ctx:Browser.CanvasRenderingContext2D),{p={x=x; y=y};s={w=w;h=h}}) =
-        ctx.drawImage(!^deck, 101.*kind,101.*suit, 101.,101.,x,y,w,h)
+        ctx.drawImage(!^Card.back,x,y,w,h)
+    member t.DrawTo((ctx:Browser.CanvasRenderingContext2D),({p={x=x; y=y};s={w=w;h=h}} as rect)) =
+        ctx.drawImage(!^Card.deck, 101*t.Kind|>float,101*t.Suit |> float, 101.,101.,x,y,w,h)
         if t.IsSelected then
             ctx.save()
             ctx.fillStyle <- !^"rgba(255, 246, 116, 0.5)"
             ctx.fillRect(x,y,w,h)
             ctx.restore()
-    member t.OnClick = t.Toggle()
-let deck = Array.init 52 Card
+        {t with LastRendering=rect}
+    member t.OnClick x y = if inside t.LastRendering {x=x;y=y} then Some t.Toggle else None
+    static member Create n = {Kind=n%13;Suit=n/13;Selected=false;LastRendering=Rect.Empty}
+let deck = Array.init 52 Card.Create
 type Deck =         
     static member NewShuffle() =
         let r = System.Random()
@@ -69,9 +79,71 @@ let spreadOn n {x=x;y=y} {h=h;w=w} =
     List.init n (fun (Float n) ->{p={x=x+x'*n;y=y+y'*n};s={h=s;w=s}})
 
 
+type Player =
+    {
+        Hand: Card list
+        Order: int
+        SelectedCards: (Card*Card) option
+        LastRendering: float Rect
+        IsHuman: bool
+    }
+    static member Create n = {Hand=[];Order=n; SelectedCards=None; LastRendering=Rect.Empty; IsHuman = true}
+    member t.DrawTo((ctx:Browser.CanvasRenderingContext2D),({p={x=x; y=y} as p;s={w=w;h=h} as s} as rect)) =
+        
+        {t with LastRendering=rect;Hand=t.Hand |> List.zip (spreadOn (List.length t.Hand) p s) |> List.map (fun (r,e)->e.DrawTo(ctx,r))}
 
-type Game() =
-    member t.Start() = ()
+type GameState =
+    {
+        Deck:Card list
+        TeamOne: int
+        TeamTwo: int
+        Round: int []
+        Table: Card list
+        PlayerToPlay:Player
+        Players: Player[]
+    }    
+    static member Empty =
+        let players = Array.init 4 Player.Create
+        {Deck=Deck.NewShuffle();TeamOne=0;TeamTwo=0;Round=Array.zeroCreate 3;Table=[];PlayerToPlay=players.[0];Players=players}
+
+type DrawingMessages =
+    | DrawCanvas of GameState*AsyncReplyChannel<GameState>
+
+type GameMessages =
+    | Reset
+    | Draw
+    | PlayCards of Player*Card[]
+    | SendClick of x:float*y:float
+    
+let drawing = MailboxProcessor.Start <| fun n ->
+    let rec proc (body:Browser.HTMLBodyElement) (canvas:Browser.HTMLCanvasElement) = async{
+        let! DrawCanvas (g,r) = n.Receive()
+        let ctx = canvas.getContext_2d()
+        let gp' = g.Players |> Array.map (fun t-> t.DrawTo(ctx, Rect.Empty))        
+        r.Reply {g with Players=gp'}
+        return! proc body canvas
+    }
+    proc (Browser.document.body :?> Browser.HTMLBodyElement) (Browser.document.getElementsByTagName_canvas().[0])
+
+    
+
+let rec game = MailboxProcessor.Start <| fun n ->
+    let rec proc g =
+        async {
+            let! m = n.Receive()
+            match m with
+            | Draw ->
+                printfn "Draw called"
+                let! g = drawing.PostAndAsyncReply (fun n -> DrawCanvas (g,n))
+                return! proc g
+            | Reset -> failwith "Not Implemented"
+            | PlayCards(_, _) -> failwith "Not Implemented"
+            | SendClick(x, y) ->                 
+                printfn "%f %f" x y
+            return! proc g
+        }
+    proc GameState.Empty
+
 
 let draw() = [
     let canvas = Browser.document.getElementsByTagName_canvas().[0]
@@ -83,13 +155,15 @@ let draw() = [
     let s'={h=s;w=s}
     let ctx = canvas.getContext_2d()
     
-    spreadOn n {x=0.;y=0.} {h=canvas.height;w=canvas.width}
+    yield! spreadOn n {x=0.;y=0.} {h=canvas.height;w=canvas.width}
     |> List.zip (Deck.NewShuffle())
-    |> List.iter (fun (c,n)-> c.DrawTo(ctx,n))   
+    |> List.map (fun (c,n)-> c.DrawTo(ctx,n))   
 ]
 let init() =
-    let body = Browser.document.body :?> Browser.HTMLBodyElement    
-    body.onresize <- fun _ -> draw() :> obj
+    let body = Browser.document.body :?> Browser.HTMLBodyElement 
+    let canvas = Browser.document.getElementsByTagName_canvas().[0]
+    canvas.onclick <- fun e -> game.Post (SendClick (e.clientX, e.clientY))  |> box
+    body.onresize <- fun _ -> game.Post Draw :> obj
     body.onload <- fun _ -> draw() :> obj
     
     
